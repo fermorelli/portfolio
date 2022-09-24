afaa0a6525182c37fb3855c57cca1df85bc8c3fc
ilcosme <fer.cmorelli@gmail.com>
2022-09-23 21:12:48 -0300

    header and some assets

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,19 @@
-import styles from './header.module.css';
+import './header.module.css';
+import { CTA } from './cta';
 
 export const Header = ()=>{
     return (
-        <div className={styles.header}>header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Fernando Morelli</h1>
+                <h5 className="text-light">a front end trainee developer</h5>
+                <CTA />
+
+                <div className='me'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </header>
     )
 };
---
+++ src/components/header/cta.jsx
@@ -0,0 +1,10 @@
+import CV from '../../assets/CV.pdf'
+
+export const CTA = () =>{
+    return (
+        <div className="cta">
+            <a href={CV} download className='btn'>Download CV</a>
+            <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+    )
+}
