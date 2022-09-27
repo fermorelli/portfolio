b8e9969187813b79b5107d4ad0f5086985c4263a
ilcosme <fer.cmorelli@gmail.com>
2022-09-27 19:46:07 -0300

    knowledge section

--- src/components/knowledge/Knowledge.jsx
+++ src/components/knowledge/Knowledge.jsx
@@ -1,7 +1,52 @@
-import styles from './knowledge.module.css';
+import './knowledge.css';
+import {BsPatchCheckFill} from 'react-icons/bs';
 
 export const Knowledge = ()=>{
     return (
-        <section id='knowledge' className={styles.knowledge}>Knowledge</section>
+        <section id='knowledge' className='knowledge'>
+            <h5>What Skills I have</h5>
+            <h2>My Knowledge</h2>
+
+            <div className="container knowledge__container">
+                <div className="knowledge__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="knowledge__content">
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>HTML</h4>
+                        </div>
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>CSS</h4>
+                        </div>
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>JavaScript</h4>
+                        </div>
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>React JS</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="knowledge__backend">
+                <h3>Backend Development</h3>
+                    <div className="knowledge__content">
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>Node JS</h4>
+                        </div>
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>Express</h4>
+                        </div>
+                        <div className='knowledge__details'>
+                            <BsPatchCheckFill className='knowledge__details-icons' />
+                            <h4>Mongo DB</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 };
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,20 @@
-import styles from './projects.module.css';
+/* eslint-disable react/jsx-no-target-blank */
+import './projects.module.css';
 
 export const Projects = ()=>{
     return (
-        <section id='projects' className={styles.projects}>projects</section>
+        <section id='projects' className='projects'>
+            <h5>My recent work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <h3>This is a portfolio item title</h3>
+                        <a href="https://github.com" className='btn' target='_blank'>Github</a>
+                        <a href="https://github.com" target='_blank' className='btn btn-primary'>Deploy</a>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 };
