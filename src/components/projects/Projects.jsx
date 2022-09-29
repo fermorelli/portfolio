a8b427ec557318bfb7cf22bad95c14fa1c2a80c4
ilcosme <fer.cmorelli@gmail.com>
2022-09-29 15:10:51 -0300

    swiper implemented

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,13 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
 import './projects.css';
-import ECM1 from '../../assets/ecommerce2.jpg';
-import ECM2 from  '../../assets/ecommerce.jpg';
-import ECM3 from  '../../assets/ecommerce3.jpg';
-import ECM4 from  '../../assets/ecommerce4.jpg';
-import TGX1 from '../../assets/trackgenix1.jpeg';
-import TGX2 from '../../assets/trackgenix2.jpeg';
-import TGX3 from '../../assets/trackgenix3.jpeg';
-import CAL from '../../assets/calculadora.png';
 import { ImageSwiper } from './imageSwiper';
 
 export const Projects = ()=>{
@@ -15,7 +7,6 @@ export const Projects = ()=>{
     const projects = [
         {
             id: 1,
-            images: ECM2,
             title: 'E-commerce',
             description: 'Virtual store to browse and buy products, with a shopping cart and a credit card and shipping info form. Frontend made with react js, consuming an API to show the different items.',
             github: 'https://github.com/ilcosme/E-commerce',
@@ -23,7 +14,6 @@ export const Projects = ()=>{
         },
         {
             id: 2,
-            images: CAL,
             title: 'Calculator',
             description: 'Functional calculator made from scratch with plain HTML, CSS and JS. Able to resolve the basics operation a normal calculator can handle. Style made entirely with plain css. ',
             github: 'https://github.com/ilcosme/CalculadoraVainilla',
@@ -31,7 +21,6 @@ export const Projects = ()=>{
         },
         {
             id: 3,
-            images: TGX1,
             title: 'Trackgenix app',
             description: 'App made with a development team to provide a schedule tracking service to companies. Frontend made with react js. Backend built with node js, mongo db and express',
             github: 'https://github.com/ilcosme/Trackgenix-app',
@@ -45,11 +34,11 @@ export const Projects = ()=>{
             <h2>Portfolio</h2>
             <div className="container portfolio__container">
                 {
-                    projects.map(({id, images, title, description, github, demo}) => {
+                    projects.map(({id, title, description, github, demo}) => {
                         return (
                             <article key={id} className='portfolio__item'>
                                 <div className='portfolio__item-image'>
-                                    <img src={images} alt={title} />
+                                    <ImageSwiper id={id} />
                                 </div>
                                 <div className="portfolio__item-text">
                                     <h3>{title}</h3>
@@ -64,7 +53,6 @@ export const Projects = ()=>{
                     })
                 }
             </div>
-            <ImageSwiper />
         </section>
     )
 };
--- src/components/projects/imageSwiper.jsx
+++ src/components/projects/imageSwiper.jsx
@@ -1,8 +1,9 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 import { Navigation, EffectFade } from 'swiper';
+import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
+import './imageSwiper.css';
 
 import ECM1 from '../../assets/ecommerce2.jpg';
 import ECM2 from  '../../assets/ecommerce.jpg';
@@ -13,9 +14,10 @@ import TGX2 from '../../assets/trackgenix2.jpeg';
 import TGX3 from '../../assets/trackgenix3.jpeg';
 import CAL from '../../assets/calculadora.png';
 
-export const ImageSwiper = () => {
+export const ImageSwiper = ({ id }) => {
+    if(id===1){
     return (
-        <div>
+        <div className='swiper__container'>
             <Swiper modules={[Navigation, EffectFade]}
             navigation
             effect
@@ -39,5 +41,52 @@ export const ImageSwiper = () => {
                 </SwiperSlide>
             </Swiper>
         </div>
-    );
+    )}else if(id===2){
+        return(
+        <div className='swiper__container'>
+        <Swiper modules={[Navigation, EffectFade]}
+        navigation
+        effect
+        speed={800}
+        slidesPerView={1}
+        loop
+        className='my__swiper'
+        >
+            <SwiperSlide className='slider'>
+                <img src={CAL} alt="" />
+            </SwiperSlide>
+            <SwiperSlide className='slider'>
+                <img src={CAL} alt="" />
+            </SwiperSlide>
+            <SwiperSlide className='slider'>
+                <img src={CAL} alt="" />
+            </SwiperSlide>
+            <SwiperSlide className='slider'>
+                <img src={CAL} alt="" />
+            </SwiperSlide>
+        </Swiper>
+    </div>
+    )}else if(id===3){
+        return(
+            <div className='swiper__container'>
+            <Swiper modules={[Navigation, EffectFade]}
+            navigation
+            effect
+            speed={800}
+            slidesPerView={1}
+            loop
+            className='my__swiper'
+            >
+                <SwiperSlide className='slider'>
+                    <img src={TGX1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide className='slider'>
+                    <img src={TGX2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide className='slider'>
+                    <img src={TGX3} alt="" />
+                </SwiperSlide>
+            </Swiper>
+            </div>
+        )};
 };
