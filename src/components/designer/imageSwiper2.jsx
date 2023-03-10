aec2d0d30f3b87639769b7e445da7fbb6e543eed
fermorelli <fer.cmorelli@gmail.com>
2023-03-10 14:14:21 -0300

    removed swiper from designer projects

--- src/components/designer/imageSwiper2.jsx
+++ src/components/designer/imageSwiper2.jsx
@@ -1,8 +1,3 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, EffectFade } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/effect-fade';
 import '../projects/imageSwiper.css';
 import skull from '../../assets/skull.jpg'
 import cd from '../../assets/jelly-cd.jpg'
@@ -12,50 +7,17 @@ export const ImageSwiper = ({ id }) => {
     if(id===2){
     return (
         <div className='swiper__container'>
-            <Swiper modules={[Navigation, EffectFade]}
-            navigation
-            effect
-            speed={800}
-            slidesPerView={1}
-            loop
-            className='my__swiper'
-            >
-                <SwiperSlide className='slider'>
-                    <img src={skull} alt="" />
-                </SwiperSlide>
-            </Swiper>
+            <img src={skull} alt="" />
         </div>
     )}else if(id===3){
         return(
             <div className='swiper__container'>
-            <Swiper modules={[Navigation, EffectFade]}
-            navigation
-            effect
-            speed={800}
-            slidesPerView={1}
-            loop
-            className='my__swiper'
-            >
-                <SwiperSlide className='slider'>
-                    <img src={cd} alt="" />
-                </SwiperSlide>
-            </Swiper>
+                <img src={cd} alt="" />
             </div>
         )}else if(id===1){
             return(
                 <div className='swiper__container'>
-                <Swiper modules={[Navigation, EffectFade]}
-                navigation
-                effect
-                speed={800}
-                slidesPerView={1}
-                loop
-                className='my__swiper'
-                >
-                    <SwiperSlide className='slider'>
-                        <img src={mj} alt="" />
-                    </SwiperSlide>
-                </Swiper>
+                    <img src={mj} alt="" />
                 </div>
         )};
 };
